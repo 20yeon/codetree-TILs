7ba1572aa467749a3b9dee3d466ac64f2d1e3d5a
20yeon <127908102+20yeon@users.noreply.github.com>
2023-12-12 10:27:45 +0900

    Title: 원형 순열에서의 인원 제거, Time: 39ms, Memory: 0MB, Status: Passed - CodeTree

--- 231212/원형 순열에서의 인원 제거/josephus-permutations.cpp
+++ 231212/원형 순열에서의 인원 제거/josephus-permutations.cpp
@@ -12,7 +12,7 @@ int main() {
     cin>>n>>k;
     for(int i = 1; i <= n; i++) circle_q.push(i);
     while(circle_q.size()){
-        for(int i = 0; i < 3; i++){
+        for(int i = 0; i < k-1; i++){
             circle_q.push(circle_q.front()); circle_q.pop();
         }
         cout<<circle_q.front()<<" ";
