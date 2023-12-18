a0dceaff95e166783aab443bb3f576be8dc7608b
20yeon <127908102+20yeon@users.noreply.github.com>
2023-12-18 14:20:06 +0900

    Title: 행복한 수열의 개수, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

--- 231218/행복한 수열의 개수/number-of-happy-sequence.cpp
+++ 231218/행복한 수열의 개수/number-of-happy-sequence.cpp
@@ -17,16 +17,14 @@ int main() {
     for(int i = 0; i < n; i++){
         int check_x = 1, check_y = 1;
         for(int j = 0; j < n-1; j++){
-            if(graph[i][j] == graph[i][j+1]) {
-                check_x++;
-                if(check_x >= m) {res++; break;}
-            }else check_x = 1;
+            if(graph[i][j] == graph[i][j+1])check_x++;
+            else check_x = 1;
+            if(check_x >= m) {res++; break;}
         }
         for(int j = 0; j < n-1; j++){
-            if(graph[j][i] == graph[j+1][i]) {
-                check_y++;
-                if(check_y >= m) {res++; break;}
-            }else check_y = 1;
+            if(graph[j][i] == graph[j+1][i])check_y++;
+            else check_y = 1;
+            if(check_y >= m) {res++; break;}
         }
     }
     cout<<res;
