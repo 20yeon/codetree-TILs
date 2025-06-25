0c24c1cec40cf527b4a250bed3b86c79638a3980
20yeon <127908102+20yeon@users.noreply.github.com>
2025-06-25 14:46:56 +0900

    Title: 두 수의 거듭제곱, Time: 8ms, Memory: 0MB, Status: Wrong Answer - Codetree

---
+++ 250625/두 수의 거듭제곱/two-numbers-of-squares.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+int a, b;
+
+int main() {
+    cin >> a >> b;
+    cout<<pow(a,b);
+    // Please write your code here.
+
+    return 0;
+}
