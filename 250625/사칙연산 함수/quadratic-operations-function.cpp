b619c33c7bb2b91910668be0e05e478bee868c85
20yeon <127908102+20yeon@users.noreply.github.com>
2025-06-25 14:59:29 +0900

    Title: 사칙연산 함수, Time: 8ms, Memory: 0MB, Status: Wrong Answer - Codetree

--- 250625/사칙연산 함수/quadratic-operations-function.cpp
+++ 250625/사칙연산 함수/quadratic-operations-function.cpp
@@ -8,7 +8,7 @@ char o;
 void solve(int a, int b, char c){
     if(c == '+') cout<<a<<" + "<<b<<" = "<<a+b;
     else if(c == '*') cout<<a<<" * "<<b<<" = "<<a*b;
-    else if(c == '/') cout<<a<<" / "<<b<<" = "<<a/b;
+    else if(c == '/') cout<<a<<" / "<<b<<" = "<<(int)a/b;
     else if(c == '-') cout<<a<<" - "<<b<<" = "<<a-b;
     else cout<<"false";
 }
