f50dac38fe2e1615792570785141b2fca5bea3c3
20yeon <127908102+20yeon@users.noreply.github.com>
2025-06-25 13:02:05 +0900

    Title: 세 실수의 반올림, Time: 7ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 250625/세 실수의 반올림/rounding-of-three-actual-numbers.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    double n1, n2, n3;
+    cin>>n1>>n2>>n3;
+
+    cout<<fixed;
+    cout.precision(3);
+
+    cout<<n1<<"\n"<<n2<<"\n"<<n3;
+    // Please write your code here.
+    return 0;
+}
