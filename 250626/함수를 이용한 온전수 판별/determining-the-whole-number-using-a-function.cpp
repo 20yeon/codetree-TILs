41c72a734d51943b3668288f74c60e68e40cec39
20yeon <127908102+20yeon@users.noreply.github.com>
2025-06-26 13:04:24 +0900

    Title: 함수를 이용한 온전수 판별, Time: 7ms, Memory: 0MB, Status: Passed - Codetree

--- 250626/함수를 이용한 온전수 판별/determining-the-whole-number-using-a-function.cpp
+++ 250626/함수를 이용한 온전수 판별/determining-the-whole-number-using-a-function.cpp
@@ -7,7 +7,8 @@ int a, b;
 void solve(int a, int b){
     int cnt = 0;
     for(int i = a; i <= b; i++){
-        if(i%2 == 0 && (i%10) == 5 && (i%3 == 0 && i%9 != 0)) cnt++;
+        if(i%2 == 0 || (i%10) == 5 || (i%3 == 0 && i%9 != 0)) continue;
+        else cnt++;
     }
     cout<<cnt;
 }
